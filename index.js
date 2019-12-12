71bcfab69969b35cf3d5c5640aeede5a1889554e
Dipankar Chakraborty <dipuchakraborty06@gmail.com>
2019-12-12 08:43:54 +0000

    Che

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+'use strict';
 const Express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
 const _application = Express();
+_application.use(bodyParser.json());
+_application.use(cookieParser());
+
+_application.post('/signin', signin);
 
 _application.listen(3000, ()=>{
     console.log("Application is running and listening on port 3000");
