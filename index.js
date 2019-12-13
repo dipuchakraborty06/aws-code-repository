3202118a9809089c8385b89c5569cdf37ef123bb
Dipankar Chakraborty <dipuchakraborty06@gmail.com>
2019-12-13 06:00:58 +0000

    Che

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 'use strict';
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
 const _application = express();
 _application.use(bodyParser.json());
+_application.use(bodyParser.urlencoded())
 _application.use(cookieParser());
 
+const stage = require('./configuration')[process.env.NODE_ENV];
 _application.get('/signin', (request, response)=>{
     var _response = {
         message: "Hello World"
     };
+    response.statusCode = 200;
+    response.send(_response);
 });
 
-var _server = _application.listen(3000, ()=>{
+var _server = _application.listen('${stage.port}', ()=>{
     var _host = _server.address().address;
-    var _port = _server.address().port;
-    console.log("Application is running and listening at http://%s:%s", _host, _port);
+    console.log('Application is running and listening at http://%s:${stage.port}', _host);
 });
+
+module.exports = _application;
